743b40f144911f223e25849e74bdc10613a59568
Ares-bit <xyx1749293900@gmail.com>
2023-10-31 23:54:27 +0800

    create main thread

--- thread/thread.c
+++ thread/thread.c
@@ -6,8 +6,25 @@
 
 #define PG_SIZE 4096
 
+struct task_struct* main_thread;
+struct list thread_ready_list;
+struct list thread_all_list;
+static struct list_elem* thread_tag;
+
+extern void switch_to(struct task_struct* cur, struct task_struct* next);
+
+//获取当前线程PCB起始地址
+struct task_struct* running_thread()
+{
+    uint32_t esp;
+    asm volatile("mov %%esp,%0": "=g"(esp));
+    return (struct task_struct*)(esp & 0xfffff000);
+}
+
 static void kernel_thread(thread_func function, void* func_arg)
 {
+    //避免后面线程无法调度
+    intr_enable();
     function(func_arg);
 }
 
@@ -29,8 +46,16 @@ void init_thread(struct task_struct* pthread, char* name, int prio)
 {
     memset(pthread, 0, sizeof(*pthread));
     strcpy(pthread->name, name);
-    pthread->status = TASK_RUNNING;
+    if (pthread == main_thread) {
+        pthread->status = TASK_RUNNING;//主线程的main一直运行
+    } else {
+        pthread->status = TASK_READY;
+    }
+
     pthread->priority = prio;
+    pthread->ticks = prio;
+    pthread->elapsed_ticks = 0;
+    pthread->pgdir = NULL;
     pthread->self_kstack = (uint32_t*)((uint32_t)pthread + PG_SIZE);
     pthread->stack_magic = 0x20001212;
 }
@@ -42,7 +67,26 @@ struct task_struct* thread_start(char* name, int prio, thread_func function, voi
     init_thread(thread, name, prio);
     //初始化线程栈
     thread_create(thread, function, func_arg);
+
+    ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
+
+    list_append(&thread_ready_list, &thread->general_tag);
+
+    ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
+
+    list_append(&thread_all_list, &thread->all_list_tag);
+/*之前为了直接跳去执行线程写的
     asm volatile("movl %0,%%esp; pop %%ebp; pop %%ebx; pop %%edi; pop %%esi; ret" :\
                 : "g"(thread->self_kstack) : "memory");
+*/
     return thread;
+}
+
+static void make_main_thread(void)
+{
+    //main thread PCB地址为0xc009e000，已经提前预留，不需要再申请
+    main_thread = running_thread();
+    init_thread(main_thread, "main", 31);
+
+    ASSERT(!elem_find(&thread_all_list, &main_thread))
 }
