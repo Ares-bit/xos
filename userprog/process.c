44ce9b4e74a396f153a4387acc694bd10fe4b113
xuxingyuan <xuxingyuan@visinex.com>
2024-03-19 17:07:45 +0800

    init user process

--- userprog/process.c
+++ userprog/process.c
@@ -3,6 +3,9 @@
 #include "memory.h"
 #include "stdint.h"
 #include "tss.h"
+#include "console.h"
+#include "string.h"
+#include "bitmap.h"
 
 extern void intr_exit(void);
 
@@ -54,4 +57,52 @@ void process_activate(struct task_struct* p_thread)
     if (p_thread->pgdir) {
         update_tss_esp(p_thread);
     }
+}
+
+//创建用户进程页目录表，并将最后一项赋值为其物理地址，方便以后修改
+uint32_t* create_page_dir(void)
+{
+    uint32_t* page_dir_vaddr = get_kernel_pages(1);
+
+    if (page_dir_vaddr == NULL) {
+        console_put_str("create_page_dir: get_kernel_pages failed!");
+        return NULL;
+    }
+
+    //此时仍是内核再运行，故0xfffff000虚拟地址会找到内核页目录表的物理地址
+    memcpy((uint32_t*)((uint32_t)page_dir_vaddr + 0x300 * 4), \
+        (uint32_t*)(0xfffff000 + 0x300 * 4), (0x400 - 0x300) * 4);
+
+    uint32_t new_page_dir_phy_addr = addr_v2p((uint32_t)page_dir_vaddr);
+    page_dir_vaddr[1023] = new_page_dir_phy_addr | PG_US_U | PG_RW_W | PG_P_1;
+
+    return page_dir_vaddr;
+}
+
+//创建用户进程虚拟地址位图
+void create_user_vaddr_bitmap(struc task_struct* user_prog)
+{
+    user_prog->userprog_vaddr.vaddr_start = USER_VADDR_START;
+    uint32_t bitmap_pg_cnt = DIV_ROUND_UP((0xc0000000 - USER_VADDR_START) / PG_SIZE / 8, PG_SIZE);
+    user_prog->userprog_vaddr.vaddr_bitmap.bits = get_kernel_pages(bitmap_pg_cnt);
+    user_prog->userprog_vaddr.vaddr_bitmap.btmp_bytes_len = (0xc0000000 - USER_VADDR_START) / PG_SIZE / 8;
+    bitmap_init(&user_prog->userprog_vaddr.vaddr_bitmap);
+}
+
+//创建并初始化用户进程
+void process_execute(void* filename, char* name)
+{
+    struct task_struct* thread = get_kernel_pages(1);
+    init_thread(thread, name, DEFAULT_PRIO);
+    create_user_vaddr_bitmap(thread);
+    thread_create(thread, start_process, filename);
+    thread->pgdir = create_page_dir();
+
+    enum intr_status old_status = intr_disable();
+    ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
+    list_append(&thread_ready_list,  &thread->general_tag);
+
+    ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
+    list_append(&thread_all_list, &thread->all_list_tag);
+    intr_set_status(old_status);
 }
