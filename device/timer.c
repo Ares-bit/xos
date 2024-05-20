63001f92009e9941f523bf16c68bc24a95b65e25
Ares-bit <xyx1749293900@gmail.com>
2024-05-20 22:54:28 +0800

    ms sleep

--- device/timer.c
+++ device/timer.c
@@ -14,8 +14,9 @@
 #define COUNTER0_MODE       2
 #define READ_WRITE_LATCH    3//先写计数初值低8位 后写高8位
 #define PIT_CONTROL_PORT    0x43
+#define MIL_SECONDS_PER_INTR    (1000 / IRQ0_FREQUENCY)//每多少毫秒发生一次时钟中断
 
-uint32_t ticks;
+uint32_t ticks;//内核自中断开启以来总共的滴答数
 
 static void frequency_set(uint8_t counter_port, uint8_t counter_no, uint8_t rwl,
                             uint8_t counter_mode, uint16_t counter_value)
@@ -41,6 +42,27 @@ static void intr_timer_handler(void)
     } else {
         cur_thread->ticks--;
     }
+}
+
+//以ticks为单位的sleep
+static void ticks_to_sleep(uint32_t sleep_ticks)
+{
+    uint32_t start_ticks = ticks;
+
+    //在sleep_ticks时间内一直让出CPU 达到sleep效果
+    while (ticks - start_ticks < sleep_ticks) {
+        thread_yield();
+    }
+}
+
+//毫秒为单位的sleep 思路为通过时钟中断将ms与ticks连接起来
+void mtime_sleep(uint32_t m_seconds)
+{
+    //通过计算m_seconds中发生了多少次时钟中断得到要休眠的ticks数（一次时钟中断加一个ticks）
+    uint32_t sleep_ticks = DIV_ROUND_UP(m_seconds, MIL_SECONDS_PER_INTR);
+    //故最少休眠10ms 比较粗糙
+    ASSERT(sleep_ticks > 0);
+    ticks_to_sleep(sleep_ticks);
 }
 
 void timer_init() {
