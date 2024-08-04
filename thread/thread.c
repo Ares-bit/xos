63ac83e980446327361fc852785d05b2bbef0efd
Ares-bit <xyx1749293900@gmail.com>
2024-08-04 15:58:40 +0800

    fs

--- thread/thread.c
+++ thread/thread.c
@@ -77,8 +77,8 @@ void init_thread(struct task_struct* pthread, char* name, int prio)
     pthread->fd_table[1] = 1;
     pthread->fd_table[2] = 2;
     uint8_t fd_idx = 3;
-    while (fs_idx < MAX_FILES_OPEN_PER_PROC) {
-        pthread->fd_table[fs_idx] = -1;
+    while (fd_idx < MAX_FILES_OPEN_PER_PROC) {
+        pthread->fd_table[fd_idx] = -1;
         fd_idx++;
     }
 
