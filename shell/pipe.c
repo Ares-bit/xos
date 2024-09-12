2c33436671a98886dd89a4534643d3d1e009293a
xuxingyuan <xuxingyuan@visinex.com>
2024-09-12 10:20:53 +0800

    pipe write data

--- shell/pipe.c
+++ shell/pipe.c
@@ -46,7 +46,7 @@ uint32_t pipe_read(int32_t fd, void* buf, uint32_t count)
     //获取管道页面环形缓冲区
     struct ioqueue* ioq = (struct ioqueue*)file_table[global_fd].fd_inode;
 
-    uint32_t ioq_len = ioq_length(ioq);
+    uint32_t ioq_len = ioq_length(ioq);//读pipe这样没问题，因为读没之后再读一个才会休眠
     //选小的数据量读取，避免阻塞
     uint32_t size = ioq_len > count ? count : ioq_len;
     while (bytes_read < size) {
@@ -67,7 +67,7 @@ uint32_t pipe_write(int32_t fd, const void* buf, uint32_t count)
     //选小的写
     //不妨假设这样一种场景：如果你的buffer容量为2，此时第一次写，长度为0，我让你写入2个字符
     //你在写第二个的时候队列就已经满了，这不就休眠了？那你怎么敢说的能保证生产者永不休眠的？
-    uint32_t ioq_left = bufsize - ioq_length(ioq);
+    uint32_t ioq_left = bufsize - ioq_length(ioq) - 1;//减1才对，比如我让你写2，队列总长就是2，只有写2-1=1才不会导致休眠
     uint32_t size = ioq_left > count ? count : ioq_left;
 
     const char* buffer = buf;
